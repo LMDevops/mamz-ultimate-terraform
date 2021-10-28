5a249f3c759f2f2a168ed7a0576764f5655c0dea
Barclay SADA <barclay.iversen@sada.com>
2021-10-28 14:09:45 -0700

    Add additional roles to impersonate SA

--- modules/bootstrap_permissions/main.tf
+++ modules/bootstrap_permissions/main.tf
@@ -15,6 +15,39 @@ resource "google_organization_iam_member" "projectCreator" {
   ]
 }
 
+resource "google_organization_iam_member" "owner" {
+  for_each = toset(var.users)
+  org_id   = var.organization_id
+  role     = "roles/owner"
+  member   = each.value
+
+  depends_on = [
+    google_organization_iam_binding.organizationAdmin
+  ]
+}
+
+resource "google_organization_iam_member" "sa-token-creator" {
+  for_each = toset(var.users)
+  org_id   = var.organization_id
+  role     = "roles/iam.serviceAccountTokenCreator"
+  member   = each.value
+
+  depends_on = [
+    google_organization_iam_binding.organizationAdmin
+  ]
+}
+
+resource "google_organization_iam_member" "sa-user" {
+  for_each = toset(var.users)
+  org_id   = var.organization_id
+  role     = "roles/iam.serviceAccountUser"
+  member   = each.value
+
+  depends_on = [
+    google_organization_iam_binding.organizationAdmin
+  ]
+}
+
 resource "google_organization_iam_binding" "folderCreator" {
   org_id  = var.organization_id
   role    = "roles/resourcemanager.folderCreator"
