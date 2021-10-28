afd24225632f20e7007980f98ec42090b179fd9f
Barclay SADA <barclay.iversen@sada.com>
2021-10-27 23:00:36 -0700

    Create second set of names for state project

--- modules/bootstrap_setup/locals.tf
+++ modules/bootstrap_setup/locals.tf
@@ -1,48 +1,49 @@
-resource "random_id" "main" {
-  byte_length = 2
-}
-
 locals {
   project_terraform_labels = var.labels
 
   proj_services_to_enable = [
-      "admin.googleapis.com",
-      "cloudbilling.googleapis.com",
-      "cloudidentity.googleapis.com",
-      "cloudresourcemanager.googleapis.com",
-      "compute.googleapis.com",
-      "iam.googleapis.com",
-      "logging.googleapis.com",
-      "monitoring.googleapis.com",
-      "orgpolicy.googleapis.com",
-      "servicenetworking.googleapis.com",
-      "serviceusage.googleapis.com",
-      "storage-api.googleapis.com",
-      "billingbudgets.googleapis.com",
+    "admin.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudidentity.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "orgpolicy.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "serviceusage.googleapis.com",
+    "storage-api.googleapis.com",
+    "billingbudgets.googleapis.com",
   ]
 
   tf_svc_acc_org_iam_roles = [
-      "roles/billing.admin",
-      "roles/billing.user",
-      "roles/compute.networkAdmin",
-      "roles/compute.xpnAdmin",
-      "roles/iam.securityAdmin",
-      "roles/storage.admin",
-      "roles/iam.serviceAccountAdmin",
-      "roles/logging.configWriter",
-      "roles/orgpolicy.policyAdmin",
-      "roles/resourcemanager.folderAdmin",
-      "roles/resourcemanager.projectCreator",
-      "roles/resourcemanager.projectIamAdmin",
-      "roles/resourcemanager.organizationViewer",
-      "roles/browser",
-      "roles/orgpolicy.policyAdmin",
-      "roles/iam.serviceAccountTokenCreator"
+    "roles/billing.admin",
+    "roles/billing.user",
+    "roles/compute.networkAdmin",
+    "roles/compute.xpnAdmin",
+    "roles/iam.securityAdmin",
+    "roles/storage.admin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/logging.configWriter",
+    "roles/orgpolicy.policyAdmin",
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.projectCreator",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/resourcemanager.organizationViewer",
+    "roles/browser",
+    "roles/orgpolicy.policyAdmin",
+    "roles/iam.serviceAccountTokenCreator"
   ]
 
-  resource_base_name   = "auto-tf-01-bootstrap"
-  project_display_name = "Organization Automation"
-  project_name         = "pr-${local.resource_base_name}"
-  bucket_name          = "sb-${local.resource_base_name}-${random_id.main.hex}"
-  sa_name              = "sa-${local.resource_base_name}"
+  resource_base_name         = "zzzz"
+  environment                = "b"
+  seed_project_label         = "tfseed"
+  seed_project_display_name  = "prj-${local.resource_base_name}-${local.environment}-${local.seed_project_label}"
+  seed_project_name          = "prj-${local.resource_base_name}-${local.environment}-${local.seed_project_label}"
+  state_project_label        = "tfstate"
+  state_project_display_name = "prj-${local.resource_base_name}-${local.environment}-${local.state_project_label}"
+  state_project_name         = "prj-${local.resource_base_name}-${local.environment}-${local.state_project_label}"
+  bucket_name                = "bkt-${local.environment}-${local.resource_base_name}-${local.state_project_label}-tfstate"
+  sa_name                    = "sa-${local.environment}-${local.resource_base_name}-${local.seed_project_label}-tf"
 }
