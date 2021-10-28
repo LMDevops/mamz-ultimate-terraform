6a1c51e0a8b91ba9913628c961291b07b1723712
Barclay SADA <barclay.iversen@sada.com>
2021-10-27 23:01:24 -0700

    Change iam binding to member to not overwrite any manual bindings

--- modules/bootstrap_permissions/main.tf
+++ modules/bootstrap_permissions/main.tf
@@ -4,10 +4,11 @@ resource "google_organization_iam_binding" "organizationAdmin" {
   members = var.users
 }
 
-resource "google_organization_iam_binding" "projectCreator" {
-  org_id  = var.organization_id
-  role    = "roles/resourcemanager.projectCreator"
-  members = var.users
+resource "google_organization_iam_member" "projectCreator" {
+  for_each = toset(var.users)
+  org_id   = var.organization_id
+  role     = "roles/resourcemanager.projectCreator"
+  member   = each.value
 
   depends_on = [
     google_organization_iam_binding.organizationAdmin
@@ -20,8 +21,8 @@ resource "google_organization_iam_binding" "folderCreator" {
   members = var.users
 
   depends_on = [
-    google_organization_iam_binding.projectCreator
+    google_organization_iam_member.projectCreator
   ]
 }
 
-//role = "roles/owner"
+
