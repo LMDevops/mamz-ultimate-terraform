60d914f36f5f52a71fd02a055a7362a22d913bd3
Barclay SADA <barclay.iversen@sada.com>
2021-10-28 19:00:35 -0700

    Take out code I'm not using. Deploy log/mon and svpc projects

--- 3-shared/main.tf
+++ 3-shared/main.tf
@@ -1,99 +1,28 @@
-# module "shared_billing_alerts" {
-#   source                = "../modules/shared_billing_alerts"
-#   budget                = "500"
-# }
-#
-# module "shared_billing_export" {
-#   source                = "../modules/shared_billing_export"
-# }
+module "shared_vpc_host_project" {
+  source          = "../modules/projects"
+  name            = local.svpc_project_name
+  project_id      = local.svpc_project_name
+  services        = local.svpc_service_apis
+  billing_account = var.billing_account
+  folder_id       = data.terraform_remote_state.organization.outputs.folders.Shared.name
+  labels          = local.project_terraform_labels
+}
 
+module "logging_monitoring_project" {
+  source          = "../modules/projects"
+  name            = local.log_mon_project_name
+  project_id      = local.log_mon_project_name
+  services        = local.log_mon_service_apis
+  billing_account = var.billing_account
+  folder_id       = data.terraform_remote_state.organization.outputs.folders.Shared.name
+  labels          = local.project_terraform_labels
+}
 
-
-# locals {
-#   project_terraform_labels = {
-#     user                   = var.user
-#     owner                  = var.owner
-#   }
-#
-#   service_apis = [
-#     "admin.googleapis.com",
-#     "cloudbilling.googleapis.com",
-#     "cloudidentity.googleapis.com",
-#     "cloudresourcemanager.googleapis.com",
-#     "compute.googleapis.com",
-#     "iam.googleapis.com",
-#     "logging.googleapis.com",
-#     "monitoring.googleapis.com",
-#     "orgpolicy.googleapis.com",
-#     "secretmanager.googleapis.com",
-#     "servicenetworking.googleapis.com",
-#     "serviceusage.googleapis.com",
-#     "storage-api.googleapis.com",
-#   ]
-#
-#   sec_logs_base      = "sec-logs-01-shared"
-#   sec_logs_project   = "pr-${local.sec_logs_base}"
-#   sec_images_base    = "sec-images-01-shared"
-#   sec_images_project = "pr-${local.sec_images_base}"
-#   obsr_log_base      = "obsr-log-01-prod"
-#   obsr_log_project   = "pr-${local.obsr_log_base}"
-#   net_shrd_base      = "net-shrd-01-hub"
-#   net_shrd_project   = "pr-${local.net_shrd_base}"
-#   sec_scrt_mngr_base = "scrt-mngr-01-shared"
-#   sec_scrt_mngr_project   = "pr-${local.sec_scrt_mngr_base}"
-# }
-#
-# resource "google_folder" "shared" {
-#   display_name = "Shared"
-#   parent       = "organizations/${var.organization_id}"
-# }
-#
-# module "security_logs" {
-#   source                = "../modules/projects"
-#   name                  = local.sec_logs_project
-#   project_id            = local.sec_logs_project
-#   services              = local.service_apis
-#   billing_account       = var.billing_account
-#   folder_id             = google_folder.shared.name
-#   labels                = local.project_terraform_labels
-# }
-#
-# module "security_trusted_images" {
-#   source                = "../modules/projects"
-#   name                  = local.sec_images_project
-#   project_id            = local.sec_images_project
-#   services              = local.service_apis
-#   billing_account       = var.billing_account
-#   folder_id             = google_folder.shared.name
-#   labels                = local.project_terraform_labels
-# }
-#
-# module "security_secrets_manager" {
-#   source                = "../modules/projects"
-#   name                  = local.sec_scrt_mngr_project
-#   project_id            = local.sec_scrt_mngr_project
-#   services              = local.service_apis
-#   billing_account       = var.billing_account
-#   folder_id             = google_folder.shared.name
-#   labels                = local.project_terraform_labels
-# }
-#
-# module "observability_logs" {
-#   source                = "../modules/projects"
-#   name                  = local.obsr_log_project
-#   project_id            = local.obsr_log_project
-#   services              = local.service_apis
-#   billing_account       = var.billing_account
-#   folder_id             = google_folder.shared.name
-#   labels                = local.project_terraform_labels
-# }
-#
-# module "network_shared_hub" {
-#   source                = "../modules/projects"
-#   name                  = local.net_shrd_project
-#   project_id            = local.net_shrd_project
-#   services              = local.service_apis
-#   billing_account       = var.billing_account
-#   folder_id             = google_folder.shared.name
-#   labels                = local.project_terraform_labels
-# }
+module "shared_billing_export" {
+  source                    = "../modules/shared_billing_export"
+  domain                    = "example.com"
+  log_mon_prj_id            = trimprefix(module.logging_monitoring_project.project_id, "projects/")
+  billing_admin_group_email = var.billing_admin_group_email
+  dataset_name              = "bqds-${local.environment}-zzzz-billing-data"
+  dataset_id                = "bqds_${local.environment}_zzzz_billing_data"
+}
