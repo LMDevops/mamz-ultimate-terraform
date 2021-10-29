cfb01e84bc802f7da10068d22680ceaa7ab03e40
Barclay SADA <barclay.iversen@sada.com>
2021-10-29 14:37:28 -0700

    Bring in org IAM role module

---
+++ modules/iam/org-iam/main.tf
@@ -0,0 +1,55 @@
+/*
+BILLING
+*/
+resource "google_organization_iam_member" "billing_admin_roles" {
+  for_each = toset(var.billing_admin_roles)
+  org_id   = var.org_id
+  role     = each.value
+  member   = "group:grp-gcp-zzzz-org-p-billing-admin@${var.domain}"
+}
+/*
+NETWORK
+*/
+resource "google_organization_iam_member" "network_admin_roles" {
+  for_each = toset(var.network_admin_roles)
+  org_id   = var.org_id
+  role     = each.value
+  member   = "group:grp-gcp-zzzz-org-p-network-admin@${var.domain}"
+}
+
+/*
+ORG ADMIN
+*/
+resource "google_organization_iam_member" "org_admin_roles" {
+  for_each = toset(var.organization_admin_roles)
+  org_id   = var.org_id
+  role     = each.value
+  member   = "group:grp-gcp-zzzz-org-p-org-admin@${var.domain}"
+}
+/*
+SECURITY
+*/
+resource "google_organization_iam_member" "security_admin_roles" {
+  for_each = toset(var.security_admin_roles)
+  org_id   = var.org_id
+  role     = each.value
+  member   = "group:grp-gcp-zzzz-org-p-org-security-admin@${var.domain}"
+}
+/*
+AUDITOR
+*/
+resource "google_organization_iam_member" "auditor_roles" {
+  for_each = toset(var.auditor_roles)
+  org_id   = var.org_id
+  role     = each.value
+  member   = "group:grp-gcp-zzzz-org-p-auditor@${var.domain}"
+}
+/*
+SUPPORT
+*/
+resource "google_organization_iam_member" "support_admin_roles" {
+  for_each = toset(var.support_admin_roles)
+  org_id   = var.org_id
+  role     = each.value
+  member   = "group:grp-gcp-zzzz-org-p-support-admin@${var.domain}"
+}
---
+++ modules/iam/org-iam/variables.tf
@@ -0,0 +1,84 @@
+variable "org_id" {
+
+}
+variable "domain" {}
+
+variable "billing_admin_roles" {
+  description = "A list of roles to give billing admins"
+  type        = list(string)
+  default = [
+    "roles/billing.admin"
+  ]
+}
+
+variable "network_admin_roles" {
+  description = "A list of roles to give to network admins"
+  type        = list(string)
+  default = [
+    "roles/accesscontextmanager.policyReader",
+    "roles/cloudsql.viewer",
+    "roles/compute.orgFirewallPolicyUser",
+    "roles/compute.instanceAdmin",
+    "roles/compute.networkViewer",
+    "roles/storage.legacyBucketReader",
+    "roles/compute.xpnAdmin",
+    "roles/container.clusterViewer",
+    "roles/dns.reader",
+    "roles/networkManagment.admin",
+    "roles/servicenetworking.networksAdmin"
+  ]
+}
+
+variable "organization_admin_roles" {
+  description = "A list of roles to give org admins"
+  type        = list(string)
+  default = [
+    "roles/billing.user",
+    "roles/cloudsupport.admin",
+    "roles/iam.organizationRoleAdmin",
+    "roles/iam.organizationPolicyAdmin",
+    "roles/resourceManager.folderAdmin",
+    "roles/resourceManager.projectCreator",
+    "roles/securitycenter.admin",
+    "roles/resourcemanager.organizationAdmin"
+  ]
+}
+
+variable "auditor_roles" {
+  description = "A list of roles to give org auditors"
+  type        = list(string)
+  default = [
+    "roles/iam.organizationRoleViewer",
+    "roles/logging.viewer",
+    "roles/resourcemanager.organizationViewer",
+    "roles/serviceusage.serviceUsageViewer",
+    "roles/viewer"
+  ]
+}
+
+variable "security_admin_roles" {
+  description = "A list of roles for security admins in the org"
+  type        = list(string)
+  default = [
+    "roles/bigquery.resourceViewer",
+    "roles/compute.viewer",
+    "roles/container.viewer",
+    "roles/iam.organizationRoleViewer",
+    "roles/iam.securityReviewer",
+    "roles/logging.privateLogViewer",
+    "roles/accesscontextmanager.policyAdmin",
+    "roles/accesscontextmanager.policyReader",
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.organizationViewer",
+    "roles/securitycenter.admin",
+    "roles/logging.viewer"
+  ]
+}
+
+variable "support_admin_roles" {
+  description = "A list of roles for support admins"
+  type        = list(string)
+  default = [
+    "roles/cloudsupport.admin"
+  ]
+}
