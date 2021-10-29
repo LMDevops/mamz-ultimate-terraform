63253a1deec99bd6bd5b59cde8270891c287bc6d
Barclay SADA <barclay.iversen@sada.com>
2021-10-29 14:33:50 -0700

    add iam group binding module

--- 2-organization/main.tf
+++ 2-organization/main.tf
@@ -1,6 +1,6 @@
 resource "google_folder" "folders" {
-  for_each     = toset(local.folders)
-  
+  for_each = toset(local.folders)
+
   display_name = each.value
   parent       = "organizations/${data.terraform_remote_state.bootstrap.outputs.organization_id}"
 }
@@ -9,4 +9,10 @@ module "organization_policies" {
   source = "../modules/organization_policies"
 
   organization_id = data.terraform_remote_state.bootstrap.outputs.organization_id
+}
+
+module "org_level_iam_group_bindings" {
+  source = "../modules/iam/org-iam"
+  domain = var.domain
+  org_id = var.organization_id
 }
