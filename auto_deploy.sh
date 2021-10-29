2bffd0808f0eb09bff1075807b654912181c02c7
Barclay SADA <barclay.iversen@sada.com>
2021-10-28 19:03:32 -0700

    Deploy script updates

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -43,8 +43,43 @@ data \"terraform_remote_state\" \"bootstrap\" {
 
 terraform -chdir=2-organization init
 terraform -chdir=2-organization plan
-# terraform -chdir=2-organization apply --auto-approve -var-file=../init-config.tfvars
+# for V2 ask if you want to apply changes
+terraform -chdir=2-organization apply --auto-approve
 
+# Setup tier 3 provider.tf
+
+echo "terraform {
+  backend \"gcs\" {
+    $BUCKET
+    prefix = \"tf_state_shared\"
+  }
+}
+
+provider \"google\" {
+  impersonate_service_account = data.terraform_remote_state.bootstrap.outputs.bootstrap_automation_service_account
+}
+
+data \"terraform_remote_state\" \"bootstrap\" {
+  backend = \"gcs\"
+  config = {
+    $BUCKET
+    prefix = \"tf_state_bootstrap\"
+  }
+}
+
+data \"terraform_remote_state\" \"organization\" {
+  backend = \"gcs\"
+  config = {
+    $BUCKET
+    prefix = \"tf_state_organization\"
+  }
+}" > ./3-shared/provider.tf
 # Tier 3 Shared Services
+terraform -chdir=3-shared init 
+
+####STOP HERE AND ADD THE TF SA TO THE BILLING ACCOUNT AS A BILLING USER IF USING THE TF SA.######
+
+terraform -chdir=3-shared apply --auto-approve
+
 # terraform -chdir=3-shared init -migrate-state -backend-config=../init-config.tfvars
 # terraform -chdir=3-shared apply --auto-approve -var-file=../init-config.tfvars
