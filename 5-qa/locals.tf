30de7783b440abf38d91ec92c9bf6dbc20943954
Barclay SADA <barclay.iversen@sada.com>
2021-11-01 12:53:13 -0700

    QA project resources

---
+++ 5-qa/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  env               = "q"
+  app1_project_name = "prj-zzzz-${local.env}-app1"
+
+  app1_service_apis = [
+    "compute.googleapis.com"
+  ]
+  project_terraform_labels = {
+    "env" = "qa"
+  }
+}
