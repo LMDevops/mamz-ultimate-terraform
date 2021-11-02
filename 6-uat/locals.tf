33497aad6a56ca7444efa8f1221785c7c22ee6bd
Barclay SADA <barclay.iversen@sada.com>
2021-11-02 16:45:36 -0700

    Add IAM roles to uat locals

--- 6-uat/locals.tf
+++ 6-uat/locals.tf
@@ -8,4 +8,32 @@ locals {
   project_terraform_labels = {
     "env" = "uat"
   }
+
+  admin_roles = [
+    "roles/viewer"
+  ]
+
+  developer_roles = [
+    "roles/compute.viewer",
+    "roles/container.viewer",
+    "roles/logging.viewer",
+    "roles/dataproc.viewer",
+    "roles/bigquery.dataViewer",
+    "roles/bigquery.resourceViewer",
+    "roles/cloudfunctions.viewer",
+    "roles/monitoring.viewer",
+    "roles/storage.legacyBucketReader"
+  ]
+
+  devops_roles = [
+    "roles/compute.viewer",
+    "roles/container.viewer",
+    "roles/logging.viewer",
+    "roles/dataproc.viewer",
+    "roles/bigquery.dataViewer",
+    "roles/bigquery.resourceViewer",
+    "roles/cloudfunctions.viewer",
+    "roles/monitoring.viewer",
+    "roles/storage.legacyBucketReader"
+  ]
 }
