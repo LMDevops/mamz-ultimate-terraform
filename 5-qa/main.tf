8e1facbabe9be49befab9eeb1ee8a23a9c3b3c57
Barclay SADA <barclay.iversen@sada.com>
2021-11-02 16:45:23 -0700

    Update qa main with IAM modules

--- 5-qa/main.tf
+++ 5-qa/main.tf
@@ -6,4 +6,27 @@ module "app1_qa_project" {
   billing_account = var.billing_account
   folder_id       = data.terraform_remote_state.organization.outputs.folders.QA.name
   labels          = local.project_terraform_labels
+  has_sa          = true
+  sa_account_id   = local.app1_project_name
+}
+
+module "app1_qa_admin_iam" {
+  source           = "../modules/iam/projects-iam"
+  project_id       = trimprefix(module.app1_qa_project.project_id, "projects/")
+  admin_roles      = local.admin_roles
+  admin_group_name = var.admin_group_name
+}
+
+module "app1_qa_developer_iam" {
+  source               = "../modules/iam/projects-iam"
+  project_id           = trimprefix(module.app1_qa_project.project_id, "projects/")
+  developer_roles      = local.developer_roles
+  developer_group_name = var.developer_group_name
+}
+
+module "app1_qa_devops_iam" {
+  source            = "../modules/iam/projects-iam"
+  project_id        = trimprefix(module.app1_qa_project.project_id, "projects/")
+  devops_roles      = local.devops_roles
+  devops_group_name = var.devops_group_name
 }
