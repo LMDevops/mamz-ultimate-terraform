2d82fdb79d11c7188f2a0433ea939f6fd6c1fb7c
Barclay SADA <barclay.iversen@sada.com>
2021-11-04 14:30:06 -0700

    Fix typos in destroy script

--- destroy.sh
+++ destroy.sh
@@ -2,7 +2,7 @@
 
 terraform -chdir=7-prod destroy --auto-approve -var-file=terraform.tfvars
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 7"
   exit 1
 else
@@ -13,7 +13,7 @@ echo "" > ./7-prod/provider.tf
 
 terraform -chdir=6-uat destroy --auto-approve -var-file=terraform.tfvars
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 6"
   exit 1
 else
@@ -24,7 +24,7 @@ echo "" > ./6-uat/provider.tf
 
 terraform -chdir=5-qa destroy --auto-approve -var-file=terraform.tfvars
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 5"
   exit 1
 else
@@ -35,7 +35,7 @@ echo "" > ./5-qa/provider.tf
 
 terraform -chdir=4-dev destroy --auto-approve -var-file=terraform.tfvars
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 4"
   exit 1
 else
@@ -46,7 +46,7 @@ echo "" > ./4-dev/provider.tf
 
 terraform -chdir=3-shared destroy --auto-approve -var-file=terraform.tfvars
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 3"
   exit 1
 else
@@ -57,7 +57,7 @@ echo "" > ./3-shared/provider.tf
 
 terraform -chdir=2-organization destroy --auto-approve -var-file=terraform.tfvars
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 2"
   exit 1
 else
@@ -67,19 +67,20 @@ fi
 echo "" > ./2-organization/provider.tf
 
 echo "" > ./1-bootstrap/provider.tf
-terraform -chdir=1-bootstrap init -migrate-state
+# terraform -chdir=1-bootstrap init -migrate-state
+terraform -chdir=1-bootstrap init -force-copy
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 1 migrating state"
   exit 1
 else
   echo "Bootstrap state migrated. Destroying resources... "
 fi
 
-terraform -chdir=1-bootstrap destroy --auto-approve -var-file=terraform.tfvars
+terraform -chdir=1-bootstrap destroy -var-file=terraform.tfvars --auto-approve 
 
 
-if [ $? !=0 ]; then
+if [ $? != 0 ]; then
   echo "Error on step 1"
   exit 1
 else
