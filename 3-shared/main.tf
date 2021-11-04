7bc6a0c0ada6532bb1551b9bfbdce3c850394abe
Barclay SADA <barclay.iversen@sada.com>
2021-11-04 14:25:46 -0700

    Naming convention updates for network

--- 3-shared/main.tf
+++ 3-shared/main.tf
@@ -1,7 +1,7 @@
 module "shared_vpc_host_project" {
   source          = "../modules/projects"
-  name            = var.svpc_project_name
-  project_id      = var.svpc_project_name
+  name            = "prj-${local.business_code}-${local.environment}-${local.svpc_project_label}"
+  project_id      = "prj-${local.business_code}-${local.environment}-${local.svpc_project_label}"
   services        = local.svpc_service_apis
   billing_account = var.billing_account
   folder_id       = data.terraform_remote_state.organization.outputs.folders.Shared.name
@@ -14,14 +14,18 @@ module "svpc_network" {
 
   project_id      = trimprefix(module.shared_vpc_host_project.project_id, "projects/")
   prefix          = "sb"
-  environment     = "s"
+  environment     = local.environment
+  vpc_type        = local.vpc_type
   network_configs = local.svpc__network_configs
+  depends_on = [
+    module.shared_vpc_host_project
+  ]
 }
 
 module "logging_monitoring_project" {
   source          = "../modules/projects"
-  name            = var.log_mon_project_name
-  project_id      = var.log_mon_project_name
+  name            = "prj-${local.business_code}-${local.environment}-${local.log_mon_project_label}"
+  project_id      = "prj-${local.business_code}-${local.environment}-${local.log_mon_project_label}"
   services        = local.log_mon_service_apis
   billing_account = var.billing_account
   folder_id       = data.terraform_remote_state.organization.outputs.folders.Shared.name
@@ -43,6 +47,7 @@ module "org_vpc_flow_log_bucket" {
   bucket_id                = "bkt-s-zzzz-log-mon-vpcflow"
   log_sink_writer_identity = module.org_vpc_flow_log_sink.writer_identity
   retention_days           = 30
+
 }
 
 module "org_vpc_flow_log_sink" {
@@ -52,6 +57,7 @@ module "org_vpc_flow_log_sink" {
   parent_resource_id   = data.terraform_remote_state.bootstrap.outputs.organization_id
   filter               = "logName:(\"projects/${trimprefix(module.logging_monitoring_project.project_id, "projects/")}/logs/compute.googleapis.com%2Fvpc_flows\")"
   destination_uri      = module.org_vpc_flow_log_bucket.destination_uri
+
 }
 
 module "shared_vpc_iam_bindings" {
