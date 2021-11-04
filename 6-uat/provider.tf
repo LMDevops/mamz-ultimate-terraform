844272435b12e2c415ce21f97fe0248d6e4c295e
Barclay SADA <barclay.iversen@sada.com>
2021-11-04 14:26:16 -0700

    Remove provider lines for automation

--- 6-uat/provider.tf
+++ 6-uat/provider.tf
@@ -1,15 +1,1 @@
-terraform {
-  backend "gcs" {
-    bucket = "bkt-b-zzzz-tfstate-tfstate"
-    prefix = "tf_state_uat"
-  }
-}
-
-data "terraform_remote_state" "organization" {
-  backend = "gcs"
-  config = {
-    bucket = "bkt-b-zzzz-tfstate-tfstate"
-    prefix = "tf_state_organization"
-  }
-}
 
