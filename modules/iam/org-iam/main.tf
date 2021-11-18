e256f1fb01ed4a1b709d52cd08aba5b1e33fc8d7
Barclay SADA <barclay.iversen@sada.com>
2021-11-18 13:03:16 -0800

    Take domain out of group name in module

--- modules/iam/org-iam/main.tf
+++ modules/iam/org-iam/main.tf
@@ -5,7 +5,7 @@ resource "google_organization_iam_member" "billing_admin_roles" {
   for_each = toset(var.billing_admin_roles)
   org_id   = var.org_id
   role     = each.value
-  member   = "group:${var.billing_admin_group}@${var.domain}"
+  member   = "group:${var.billing_admin_group}"
 }
 /*
 NETWORK
@@ -14,7 +14,7 @@ resource "google_organization_iam_member" "network_admin_roles" {
   for_each = toset(var.network_admin_roles)
   org_id   = var.org_id
   role     = each.value
-  member   = "group:${var.network_admin_group}${var.domain}"
+  member   = "group:${var.network_admin_group}"
 }
 
 /*
@@ -24,7 +24,7 @@ resource "google_organization_iam_member" "org_admin_roles" {
   for_each = toset(var.organization_admin_roles)
   org_id   = var.org_id
   role     = each.value
-  member   = "group:${var.org_admin_group}@${var.domain}"
+  member   = "group:${var.org_admin_group}"
 }
 /*
 SECURITY
@@ -33,7 +33,7 @@ resource "google_organization_iam_member" "security_admin_roles" {
   for_each = toset(var.security_admin_roles)
   org_id   = var.org_id
   role     = each.value
-  member   = "group:${var.security_admin_group}@${var.domain}"
+  member   = "group:${var.security_admin_group}"
 }
 /*
 AUDITOR
@@ -42,7 +42,7 @@ resource "google_organization_iam_member" "auditor_roles" {
   for_each = toset(var.auditor_roles)
   org_id   = var.org_id
   role     = each.value
-  member   = "group:${var.auditor_group}@${var.domain}"
+  member   = "group:${var.auditor_group}"
 }
 /*
 SUPPORT
@@ -51,5 +51,5 @@ resource "google_organization_iam_member" "support_admin_roles" {
   for_each = toset(var.support_admin_roles)
   org_id   = var.org_id
   role     = each.value
-  member   = "group:${var.support_admin_group}@${var.domain}"
+  member   = "group:${var.support_admin_group}"
 }
