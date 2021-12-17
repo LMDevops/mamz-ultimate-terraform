a245f05d51fe1c756b31232731ac64e05f1f123a
Stacy Véronneau <88889305+sveronneau-sada@users.noreply.github.com>
2021-12-16 21:01:05 -0500

    Update locals.tf

--- 4-dev/locals.tf
+++ 4-dev/locals.tf
@@ -1,7 +1,7 @@
 locals {
   env               = "d"
-  app_name          = "app1" # Limit to 6 characters
-  business_code     = "zzzz" # Limit to 4-6 characters
+  app_name          = "app1" # APP_CHANGE_ME - Limit to 6 characters
+  business_code     = "zzzz" # BC_CHANGE_ME  - Limit to 4-6 caracters
   app1_project_name = "prj-${local.business_code}-${local.env}-${local.app_name}"
 
   app1_service_apis = [
