dfb72928c40a419c64748ca21475745144b887bc
Barclay SADA <barclay.iversen@sada.com>
2022-02-24 14:11:49 -0800

    Group creation script

---
+++ create_groups.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+
+import os.path
+import requests
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/admin.directory.group']
+
+
+def main():
+    """Create groups required for foundation deployment"""
+    creds = None
+    print('main called')
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'desktop-creds.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    # service = build('admin', 'directory_v1', credentials=creds)
+
+    # Call the Admin SDK Directory API
+    creds = creds.to_json()
+    creds = json.loads(creds)
+
+    groups = [
+        'gcp-billing-admins',
+        'gcp-network-admins',
+        'gcp-organization-admins',
+        'gcp-auditors',
+        'gcp-security-admins',
+        'gcp-support-admins'
+    ]
+    for i in range(0, len(groups)):
+
+        data = {
+            "email": groups[i] + "@cf-0003.sadaess.com",
+            "name": groups[i],
+            "aliases": [
+            ],
+            "nonEditableAliases": [
+            ]
+        }
+        headers = {"Authorization": "Bearer " + creds['token']}
+        result = requests.post(
+            'https://admin.googleapis.com/admin/directory/v1/groups', json=data, headers=headers)
+
+        result = json.loads(result.text)
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
