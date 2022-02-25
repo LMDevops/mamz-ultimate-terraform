4e3dfaafd345b7a496ea22b2cd6e906b7fd81d42
Jimmy Cui <jimmy.cui@sada.com>
2022-02-25 23:28:22 +0100

    add condition to org-iam roles to make them immutable

--- modules/iam/org-iam/main.tf
+++ modules/iam/org-iam/main.tf
@@ -2,7 +2,8 @@
 BILLING
 */
 resource "google_organization_iam_member" "billing_admin_roles" {
-  for_each = toset(var.billing_admin_roles)
+  /* this resource will not be provisioned if the group name is not given */
+  for_each = var.billing_admin_group != "" ? toset(var.billing_admin_roles) : []  
   org_id   = var.org_id
   role     = each.value
   member   = "group:${var.billing_admin_group}"
@@ -11,7 +12,8 @@ resource "google_organization_iam_member" "billing_admin_roles" {
 NETWORK
 */
 resource "google_organization_iam_member" "network_admin_roles" {
-  for_each = toset(var.network_admin_roles)
+  /* this resource will not be provisioned if the group name is not given */
+  for_each = var.network_admin_group != "" ? toset(var.network_admin_roles) : []  
   org_id   = var.org_id
   role     = each.value
   member   = "group:${var.network_admin_group}"
@@ -21,7 +23,8 @@ resource "google_organization_iam_member" "network_admin_roles" {
 ORG ADMIN
 */
 resource "google_organization_iam_member" "org_admin_roles" {
-  for_each = toset(var.organization_admin_roles)
+  /* this resource will not be provisioned if the group name is not given */
+  for_each = var.org_admin_group != "" ? toset(var.organization_admin_roles) : []   
   org_id   = var.org_id
   role     = each.value
   member   = "group:${var.org_admin_group}"
@@ -30,7 +33,8 @@ resource "google_organization_iam_member" "org_admin_roles" {
 SECURITY
 */
 resource "google_organization_iam_member" "security_admin_roles" {
-  for_each = toset(var.security_admin_roles)
+  /* this resource will not be provisioned if the group name is not given */
+  for_each = var.security_admin_group != "" ? toset(var.organization_admin_roles) : []   
   org_id   = var.org_id
   role     = each.value
   member   = "group:${var.security_admin_group}"
@@ -39,7 +43,8 @@ resource "google_organization_iam_member" "security_admin_roles" {
 AUDITOR
 */
 resource "google_organization_iam_member" "auditor_roles" {
-  for_each = toset(var.auditor_roles)
+  /* this resource will not be provisioned if the group name is not given */
+  for_each = var.auditor_group != "" ? toset(var.auditor_roles) : []    
   org_id   = var.org_id
   role     = each.value
   member   = "group:${var.auditor_group}"
@@ -48,7 +53,8 @@ resource "google_organization_iam_member" "auditor_roles" {
 SUPPORT
 */
 resource "google_organization_iam_member" "support_admin_roles" {
-  for_each = toset(var.support_admin_roles)
+  /* this resource will not be provisioned if the group name is not given */
+  for_each = var.support_admin_group != "" ? toset(var.support_admin_roles) : []   
   org_id   = var.org_id
   role     = each.value
   member   = "group:${var.support_admin_group}"
