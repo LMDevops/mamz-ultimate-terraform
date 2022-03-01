733eca2a1b49ccfbde7de250db9aa80272c03dda
Barclay SADA <barclay.iversen@sada.com>
2022-03-01 12:07:04 -0800

    Clean up prep script

--- 0-prep.sh
+++ 0-prep.sh
@@ -59,26 +59,6 @@ do
   fi
 done
 
-## May not be needed anymore.
-# for i in 1 2 3
-# do
-#   gcloud services enable iap.googleapis.com --project=$ADMIN_PROJECT_ID;
-
-#   if [ $? != 0 ]; then
-#     echo "*** Failed enabling IAP API, will try again up to 3 times."
-#     FAILCOUNT+= 1;
-#     if [ FAILCOUNT > 3 ]; then
-#       echo "####Error enabling IAP API####"
-#       exit 1
-#     else
-#       sleep 30
-#     fi
-#   else
-#     echo "IAP API enabled"
-#     break
-#   fi
-# done
-
 for i in 1 2 3
 do
   gcloud services enable iam.googleapis.com --project=$ADMIN_PROJECT_ID;
@@ -102,7 +82,7 @@ done
 # Create the service account that will perform Admin API calls
 ##
 
-# gcloud iam service-accounts create $ADMIN_SA --description="Used for making Workspace admin API calls" --display-name="workspace-admin-api-caller" --project=$ADMIN_PROJECT_ID 
+gcloud iam service-accounts create $ADMIN_SA --description="Used for making Workspace admin API calls" --display-name="workspace-admin-api-caller" --project=$ADMIN_PROJECT_ID 
 
 if [ $? != 0 ]; then
   echo "*** Error creating admin service account"
@@ -111,7 +91,7 @@ else
   echo "*** Service account created"
 fi
 
-# gcloud iam service-accounts keys create ./sada-cf-admin --iam-account=$ADMIN_SA@$ADMIN_PROJECT_ID.iam.gserviceaccount.com
+gcloud iam service-accounts keys create ./sa-admin-caller.p12 --key-file-type=p12 --iam-account=$ADMIN_SA@$ADMIN_PROJECT_ID.iam.gserviceaccount.com 
 
 if [ $? != 0 ]; then
   echo "*** Error creating service account keys"
@@ -120,59 +100,6 @@ else
   echo "*** Service account keys created"
 fi
 
-printf "The script has completed successfully. In order to finish provisioning the environment, please follow the instructions at: \n\nhttps://docs.google.com/document/d/12t9TsbVwGFIUc0D0I263NqgT3m_pch1PiproqfWCo0M \n"
+printf "The script has completed successfully. In order to finish provisioning the environment, please follow the instructions at: \n\nhttps://docs.google.com/document/d/12t9TsbVwGFIUc0D0I263NqgT3m_pch1PiproqfWCo0M/edit#heading=h.nfzma1molxm2 \n"
 
 exit 0
-
-# ##
-# Create the oauth app so that we can generate credentials for admin API calls
-# ##
-# gcloud alpha iap oauth-brands create --application_title="admin-sdk-app" --support_email=$ADMIN_EMAIL --project=$ADMIN_PROJECT_ID
-
-# if [ $? != 0 ]; then
-#   echo "*** Error creating brand"
-#   exit 1
-# else
-#   echo "*** Brand created"
-# fi
-
-# # printf "*** Script ran successfully. \n\n\nPlease manually create 'desktop app' credentials for the create_groups.py script by going to: \n\nhttps://console.cloud.google.com/apis/credentials?project=$ADMIN_PROJECT_ID&supportedpurview=project \n\nand clicking 'Create credentials and selecting OAuth client ID'\n\n"
-# printf "*** Attempting to create oauth client ID \n"
-
-# # Commented out for now but will be used for future automated steps
-# var=$(gcloud alpha iap oauth-brands list --project=$ADMIN_PROJECT_ID | grep name) 
-
-# BRAND=$(echo ${var##*/})
-
-# # echo $BRAND
-
-# gcloud beta iap oauth-clients create $BRAND --display_name="automatedGroup" --project=$ADMIN_PROJECT_ID
-
-# if [ $? != 0 ]; then
-#   echo "*** Error creating OAuth client"
-#   exit 1
-# else
-#   echo "*** OAuth client created"
-# fi
-
-# var=$(gcloud beta iap oauth-clients list $BRAND --project=$ADMIN_PROJECT_ID --filter="displayName: automatedGroup" | grep name)
-
-# NAME=$(echo ${var##*/})
-
-# # echo $NAME
-
-# var=$(gcloud beta iap oauth-clients list $BRAND --project=$ADMIN_PROJECT_ID --filter="displayName: automatedGroup" | grep secret)
-
-# SECRET=$(echo ${var##*:})
-
-# # echo $SECRET
-
-# echo "Setting client_id and client secret in creds.json"
-
-# cat <<EOF >./creds.json
-# {"installed":{"client_id":"$NAME","project_id":"$ADMIN_PROJECT_ID","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"$SECRET","redirect_uris":["urn:ietf:wg:oauth:2.0:oob",""]}}
-# EOF
-
-# cat <<EOF >./creds.json
-# {"web":{"client_id":"1013927195558-gn6ub008aogvpeb41dgas70cmj7qp2u4.apps.googleusercontent.com","project_id":"sada-cf-admin-prj-003","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost"],"javascript_origins":["http://localhost"]}}
-# EOF
